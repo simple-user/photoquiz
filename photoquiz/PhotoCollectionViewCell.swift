71c7fcd6ddd69aaf1b4b72b9aaae8ee9f6de1c1e
Oleksandr Kachanov <okacha@softseveinc.com>
2017-08-12 14:36:08 +0300

    Collection view

---
+++ photoquiz/PhotoCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoCollectionViewCell.swift
+//  photoquiz
+//
+//  Created by Oleksandr on 8/12/17.
+//  Copyright © 2017 Rivne Hackathon. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+
+
+    @IBOutlet var guessButton: UIButton!
+    @IBOutlet var imageView: UIImageView!
+
+    @IBAction func onGuess() {
+
+    }
+}
