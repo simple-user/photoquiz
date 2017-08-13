7f74dabdc937d4d1042b003a22340a35ce3e0449
Oleksandr Kachanov <okacha@softseveinc.com>
2017-08-13 13:59:38 +0300

    moved button

--- photoquiz/PhotoCollectionViewCell.swift
+++ photoquiz/PhotoCollectionViewCell.swift
@@ -8,21 +8,9 @@
 
 import UIKit
 
-protocol PhotoCollectionViewCellDelegate: class {
-    func onGuess(sender: UICollectionViewCell)
-}
-
 class PhotoCollectionViewCell: UICollectionViewCell {
 
 
-//    @IBOutlet var contentHeightConstraint: NSLayoutConstraint!
-//    @IBOutlet var contenetWidthConstraint: NSLayoutConstraint!
-    @IBOutlet var guessButton: UIButton!
     @IBOutlet var imageView: UIImageView!
 
-    weak var delegate: PhotoCollectionViewCellDelegate?
-
-    @IBAction func onGuess() {
-        delegate?.onGuess(sender: self)
-    }
 }
--- photoquiz/ViewController.swift
+++ photoquiz/ViewController.swift
@@ -36,13 +36,18 @@ class ViewController: UIViewController {
 
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
     @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet var guessButton: UIButton!
+    @IBOutlet var topGradient: UIImageView!
 
     override func viewDidLoad() {
         
         self.collectionView.register(UINib(nibName: "PhotoCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "photoCell")
 
         self.activityIndicator.startAnimating()
-        self.collectionView.isHidden = true
+        self.collectionView.alpha = 0.0
+        self.guessButton.alpha = 0.0
+        self.topGradient.alpha = 0.0
+
 
         dbRef = Database.database().reference()
         storage = Storage.storage()
@@ -51,9 +56,17 @@ class ViewController: UIViewController {
                 // here ready to go! :)
                 self.activityIndicator.stopAnimating()
                 self.collectionView.reloadData()
-                self.collectionView.isHidden = false
                 self.setupProgressBar(photosCount: self.currentModels.count)
-                print("vse ok")
+
+                UIView.animate(withDuration: 0.5, animations: {
+                    self.collectionView.alpha = 1.0
+                    self.guessButton.alpha = 1.0
+                    self.topGradient.alpha = 1.0
+                }, completion: { _ in
+                    self.collectionView.alpha = 1.0
+                    self.guessButton.alpha = 1.0
+                    self.topGradient.alpha = 1.0
+                })
             }
         }
     }
@@ -83,28 +96,27 @@ class ViewController: UIViewController {
         self.currentModels = getRandomModels(dummyCount: 5, trueModel: nil)
         self.currentImages = self.currentModels.map { _  in #imageLiteral(resourceName: "noimage") }
 
-        let imagesCount = min(self.currentModels.count, 2)
-        var imagesLoaded = 0
         for index in 0 ..< self.currentModels.count {
             getPhoto(fromModel: self.currentModels[index], completion: { image in
 
                 self.currentImages[index] = image
-                if index == 0 || index == 1 {
-                    imagesLoaded += 1
-                }
-
-                if imagesLoaded == imagesCount {
+                if index == 0 {
                     DispatchQueue.main.async {
                         print("readyToGo()")
                         readyToGo()
                     }
                 }
+
             })
         }
     }
 
     @IBAction func backToMenu(_ sender: Any) {
         self.parent?.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func onGuess() {
+        self.onShowMap(trueModel: currentModels[currentIndex])
     }
     
     func showNextImage() {
@@ -187,14 +199,6 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController: PhotoCollectionViewCellDelegate {
-    func onGuess(sender: UICollectionViewCell) {
-        guard let section = self.collectionView.indexPath(for: sender)?.section,
-            section < currentModels.count else { return }
-        self.onShowMap(trueModel: currentModels[section])
-    }
-}
-
 extension ViewController: UICollectionViewDataSource {
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -210,7 +214,7 @@ extension ViewController: UICollectionViewDataSource {
             as? PhotoCollectionViewCell else { return UICollectionViewCell() }
         let image = self.currentImages[indexPath.section]
         view.imageView.image = image
-        view.delegate = self
+        
 
         return view
     }
